073b1f120edbbd39ea8d4defcaea7e9184a3f6c7
Dimitrios Pritsos <dpritsos@extremepro.gr>
2015-06-18 21:00:23 +0300

    In progress, great amount of work ahead.

---
+++ djumble/hmrf_kmeans.py
@@ -0,0 +1,34 @@
+
+
+import numpy as np
+
+
+def HMRFKmeans(x_arr, costr, const_violat_w, dist_measure):
+    """HMRF Kmeans: A Semi-supervised clustering algorithm based on Hidden Markov Random Fields
+        Clustering model optimised by Expectation Maximisation (EM) algorithm with Hard clustering
+        constraints, i.e. a Kmeans Semi-supervised clustering variant.
+    """
+
+    k_centroids = init_cluster()
+
+
+def ICM():
+    """ICM: Iterated Conditional Modes (for the E-Step)
+    """
+    pass
+
+
+def ExploreAL():
+    """Explore Step for the Active Learning Phase
+    """
+    pass
+
+
+def ConsolidateAL():
+    """Consolidate Step for the Active Learning Phase
+    """
+    pass
+
+
+def InitClustering():
+    pass
