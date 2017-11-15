0b6c03e2f9f4287e7087ed5c6f7b8fa1296e74d8
Dimitrios Pritsos <dpritsos@extremepro.gr>
2017-11-15 21:07:42 +0200

    Refactoring profecess

--- djumble/dsmeasures/dsmeasures.pyx
+++ djumble/dsmeasures/dsmeasures.pyx
@@ -13,6 +13,8 @@ cimport cython
 cdef extern from "math.h":
     cdef double sqrt(double x) nogil
     cdef double acos(double x) nogil
+    cdef double pow(double x, double y) nogil
+    cdef double log (double x) nogil
 
 
 cpdef double [:, ::1] cosdis_2d(double [:, ::1] m1, double [:, ::1] m2):
@@ -151,17 +153,19 @@ cpdef double [:, ::1] eudis_2d(double [:, ::1] m1, double [:, ::1] m2):
     return eudis_vect
 
 
+# Note: Make it cdef if only for interal usage in cython.
 cpdef double vdot(double [::1] v1, double [::1] v2):
 
     # if v1.shape[0] != v2.shape[0]:
     #     raise Exception("Matrix dimensions mismatch. Dot product cannot be computed.")
 
     # Matrix index variables.
-    cdef unsigned int i
-    cdef unsigned int I = v1.shape[0]
+    cdef:
+        unsigned int i
+        unsigned int I = v1.shape[0]
 
-    # Initializing the result variable.
-    cdef double res = 0.0
+        # Initializing the result variable.
+        double res = 0.0
 
     # Calculating the dot product.
     with nogil:
@@ -170,24 +174,25 @@ cpdef double vdot(double [::1] v1, double [::1] v2):
 
     return res
 
-
+# Note: Make it cdef if only for interal usage in cython.
 cpdef double [:, ::1] dot2d(double [:, ::1] m1, double [:, ::1] m2):
 
     # if m1.shape[1] != m2.shape[0]:
     #     raise Exception("Matrix dimensions mismatch. Dot product cannot be computed.")
 
     # Matrix index variables.
-    cdef unsigned int i, j, k
-    cdef unsigned int I = m1.shape[0]
-    cdef unsigned int J = m2.shape[1]
-    cdef unsigned int K = m1.shape[1]
+    cdef:
+        unsigned int i, j, k
+        unsigned int I = m1.shape[0]
+        unsigned int J = m2.shape[1]
+        unsigned int K = m1.shape[1]
 
-    # Creating the numpy.array for results and its memory view
-    cdef double [:, ::1] res = np.zeros((I, J), dtype=np.float)
+        # Creating the numpy.array for results and its memory view
+        double [:, ::1] res = np.zeros((I, J), dtype=np.float)
 
     # Calculating the dot product.
-    with nogil:
-        for i in range(I):
+    with nogil, parallel():
+        for i in prange(I, schedule='guided'):
             for j in range(J):
                 for k in range(K):
                     res[i, j] += m1[i, k] * m2[k, j]
@@ -195,24 +200,26 @@ cpdef double [:, ::1] dot2d(double [:, ::1] m1, double [:, ::1] m2):
     return res
 
 
+# Note: Make it cdef in only for Cython interal usage.
 cpdef double [:, ::1] dot2d_2d(double [:, :] m1, double [:, :] m2):
 
     # if m1.shape[1] != m2.shape[0]:
     #     raise Exception("Matrix dimensions mismatch. Dot product cannot be computed.")
 
     # Matrix index variables.
-    cdef unsigned int i0, j0, i1, j1
-    cdef unsigned int I0 = m1.shape[0]
-    cdef unsigned int I1 = m1.shape[1]
-    # cdef unsigned int J0 = m2.shape[0]
-    cdef unsigned int J1 = m2.shape[1]
+    cdef:
+        unsigned int i0, j0, i1, j1
+        unsigned int I0 = m1.shape[0]
+        unsigned int I1 = m1.shape[1]
+        # cdef unsigned int J0 = m2.shape[0]
+        unsigned int J1 = m2.shape[1]
 
     # Creating the numpy.array for results and its memory view
     cdef double [:, ::1] res = np.zeros((I0, J1), dtype=np.float)
 
     # Calculating the dot product.
-    with nogil:
-        for i0 in range(I0):
+    with nogil, parallel():
+        for i0 in prange(I0, schedule='guided'):
             for j1 in range(J1):
                 for i1 in range(I1):
                     res[i0, j1] += m1[i0, i1] * m2[i1, j1]
@@ -220,39 +227,43 @@ cpdef double [:, ::1] dot2d_2d(double [:, :] m1, double [:, :] m2):
     return res
 
 
+# Note: Make it cdef if only for interal usage in cython.
 cpdef double [:, ::1] dot2d_ds(double [:, ::1] m1, double [::1] m2):
 
     # if m1.shape[1] != m2.shape[0]:
     #     raise Exception("Matrix dimensions mismatch. Dot product cannot be computed.")
 
     # Matrix index variables.
-    cdef unsigned int i, j
-    cdef unsigned int I = m1.shape[0]
-    cdef unsigned int J = m2.shape[0]
+    cdef:
+        unsigned int i, j
+        unsigned int I = m1.shape[0]
+        unsigned int J = m2.shape[0]
 
-    # Creating the numpy.array for results and its memory view
-    cdef double [:, ::1] res = np.zeros((I, J), dtype=np.float)
+        # Creating the numpy.array for results and its memory view
+        double [:, ::1] res = np.zeros((I, J), dtype=np.float)
 
     # Calculating the dot product.
-    with nogil:
-        for i in range(I):
+    with nogil, parallel():
+        for i in prange(I, schedule='guided'):
             for j in range(J):
                 res[i, j] = m1[i, j] * m2[j]
 
     return res
 
 
+# Note: Make it cdef if only for interal usage in cython.
 cpdef double [::1] dot1d_ds(double [::1] v, double [::1] m):
 
     # if v.shape[0] != m.shape[0]:
     #     raise Exception("Matrix dimensions mismatch. Dot product cannot be computed.")
 
     # Matrix index variables.
-    cdef unsigned int i
-    cdef unsigned int I = v.shape[0]
+    cdef:
+        unsigned int i
+        unsigned int I = v.shape[0]
 
-    # Creating the numpy.array for results and its memory view
-    cdef double [::1] res = np.zeros((I), dtype=np.float)
+        # Creating the numpy.array for results and its memory view
+        double [::1] res = np.zeros((I), dtype=np.float)
 
     # Calculating the dot product.
     with nogil:
@@ -262,15 +273,17 @@ cpdef double [::1] dot1d_ds(double [::1] v, double [::1] m):
     return res
 
 
+# Note: Make it cdef if only for interal usage in cython.
 cpdef double [::1] sum_axs0(double [:, ::1] m, cnp.intp_t [::1] idxs):
 
     # Matrix index variables.
-    cdef unsigned int i, j
-    cdef unsigned int I = idxs.shape[0]
-    cdef unsigned int J = m.shape[1]
+    cdef:
+        unsigned int i, j
+        unsigned int I = idxs.shape[0]
+        unsigned int J = m.shape[1]
 
-    # Creating the numpy.array for results and its memory view
-    cdef double [::1] res = np.zeros((J), dtype=np.float)
+        # Creating the numpy.array for results and its memory view
+        double [::1] res = np.zeros((J), dtype=np.float)
 
     # Calculating the dot product.
     with nogil:
@@ -283,14 +296,16 @@ cpdef double [::1] sum_axs0(double [:, ::1] m, cnp.intp_t [::1] idxs):
     return res
 
 
+# Note: Make it cdef if only for interal usage in cython.
 cpdef double [::1] get_diag(double [:, ::1] m):
 
     # Matrix index variables.
-    cdef unsigned int i
-    cdef unsigned int I = m.shape[0]
+    cdef:
+        unsigned int i
+        unsigned int I = m.shape[0]
 
-    # Creating the numpy.array for results and its memory view
-    cdef double [::1] res = np.zeros((I), dtype=np.float)
+        # Creating the numpy.array for results and its memory view
+        double [::1] res = np.zeros((I), dtype=np.float)
 
     # Calculating the dot product.
     with nogil:
@@ -302,6 +317,7 @@ cpdef double [::1] get_diag(double [:, ::1] m):
     return res
 
 
+# Note: Make it cdef if only for interal usage in cython.
 cpdef double [:, ::1] div2d_vv(double [:, ::1] m, double [::1] v):
 
     # _vv stands for Vertical Vector.
@@ -310,31 +326,34 @@ cpdef double [:, ::1] div2d_vv(double [:, ::1] m, double [::1] v):
     #     raise Exception("Matrix dimensions mismatch. 2D matrix cannot be dived by the vertical vector")
 
     # Matrix index variables.
-    cdef unsigned int i0, i1, j
-    cdef unsigned int I0 = m.shape[0]
-    cdef unsigned int I1 = m.shape[1]
-    cdef unsigned int J = v.shape[0]
+    cdef:
+        unsigned int i0, i1, j
+        unsigned int I0 = m.shape[0]
+        unsigned int I1 = m.shape[1]
+        unsigned int J = v.shape[0]
 
-    # Creating the numpy.array for results and its memory view
-    cdef double [:, ::1] res = np.zeros((I0, I1), dtype=np.float)
+        # Creating the numpy.array for results and its memory view
+        double [:, ::1] res = np.zeros((I0, I1), dtype=np.float)
 
     #
-    with nogil:
-        for i0 in range(I0):
+    with nogil, parallel():
+        for i0 in prange(I0, schedule='guided'):
             for i1 in range(I1):
                 res[i0, i1] = m[i0, i1] / v[i0]
 
     return res
 
 
+# Note: Make it cdef if only for interal usage in cython.
 cpdef double [::1] vdiv_num(double [::1] v, double num):
 
     # Matrix index variables.
-    cdef unsigned int i
-    cdef unsigned int I = v.shape[0]
+    cdef:
+        unsigned int i
+        unsigned int I = v.shape[0]
 
-    # Creating the numpy.array for results and its memory view
-    cdef double [::1] res = np.zeros((I), dtype=np.float)
+        # Creating the numpy.array for results and its memory view
+        double [::1] res = np.zeros((I), dtype=np.float)
 
     #
     with nogil:
@@ -344,14 +363,16 @@ cpdef double [::1] vdiv_num(double [::1] v, double num):
     return res
 
 
+# Note: Make it cdef if only for interal usage in cython.
 cpdef double [::1] vsqrt(double [::1] v):
 
     # Matrix index variables.
-    cdef unsigned int i
-    cdef unsigned int I = v.shape[0]
+    cdef:
+        unsigned int i
+        unsigned int I = v.shape[0]
 
-    # Creating the numpy.array for results and its memory view
-    cdef double [::1] res = np.zeros((I), dtype=np.float)
+        # Creating the numpy.array for results and its memory view
+        double [::1] res = np.zeros((I), dtype=np.float)
 
     #
     with nogil:
@@ -359,3 +380,44 @@ cpdef double [::1] vsqrt(double [::1] v):
             res[i] = sqrt(v[i])
 
     return res
+
+
+cpdef double PartialDerivative(cnp.intp_t a_idx, double [::1] x1, double [::1] x2, double [::1] A):
+    """ Partial Derivative: This method is calculating the partial derivative of a specific
+        parameter given the proper vectors. That is, for the cosine distance is a x_i with the
+        centroid vector (mu) of the cluster where x_i is belonging into. As for the constraint
+        violations is the x_1 and x_2 of a specific pair of constraints each time this method
+        is called.
+        **for detail see documentation.
+
+        Arguments
+        ---------
+            a_idx: The index of the parameter on the diagonal of the A diagonal sparse
+                parameters matrix.
+            x1, x2: The vectors will be used for the partial derivative calculation.
+
+        Output
+        ------
+            res_a: The partial derivative's value.
+
+    """
+    cdef double res_a = 0.0
+
+    # Calculating parametrized Norms ||Σ xi||(A)
+    x1_pnorm = sqrt(vdot(dot1d_ds(x1, A), x1))
+    x2_pnorm = sqrt(vdot(dot1d_ds(x2, A), x2))
+
+    res_a = (
+                (x1[a_idx] * x2[a_idx] * x1_pnorm * x2_pnorm) -
+                (
+                    vdot(dot1d_ds(x1, A), x2) *
+                    (
+                        (
+                            pow(x1[a_idx], 2.0) * pow(x2_pnorm, 2.0) +
+                            pow(x2[a_idx], 2.0) * pow(x1_pnorm, 2.0)
+                        ) / (2 * x1_pnorm * x2_pnorm)
+                    )
+                )
+            ) / (pow(x1_pnorm, 2.0) * pow(x2_pnorm, 2.0))
+
+    return res_a
